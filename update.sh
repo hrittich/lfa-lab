1fe72287e67ab7b856fba4cf8543c7a4cd66f680
Hannah Rittich <hrittich@users.noreply.github.com>
2020-04-03 10:12:12 +0200

    Update.

--- update.sh
+++ update.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
-WORKDIR=$(mktemp -d)
-git clone https://github.com/hrittich/lfa-lab $WORKDIR
+WORKDIR=lfa_lab
+(cd $WORKDIR; git pull)
 (cd $WORKDIR; cmake . ; make -j4 sphinx-doc)
+# copy files and delete old ones
+# keep all configuration files and files we created in this repository
 rsync -rv --delete \
   --exclude='update.sh' \
   --exclude='releases' \
+  --exclude='lfa_lab' \
   --exclude='.*' \
   $WORKDIR/doc/html/ .
-
-rm -r $WORKDIR
 

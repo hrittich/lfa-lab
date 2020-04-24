77631ac80ea265edd54f7b8074de41b6055be582
Hannah Rittich <hrittich@users.noreply.github.com>
2020-04-24 19:13:37 +0200

    Added further stencil unit test.

--- test/stencil_test.py
+++ test/stencil_test.py
@@ -3,6 +3,13 @@ from lfa_lab.stencil import *
 
 class StencilTest(unittest.TestCase):
 
+    def test_element_store(self):
+        s = SparseStencil([((0,), 1.0)])
+        self.assertEqual(s[0], ((0,), 1.0))
+
+        s = SparseStencil([((0,), 1.0j)])
+        self.assertEqual(s[0], ((0,), 1.0j))
+
     def test_multiply(self):
 
         # test whether to applications of the approximation of the first
@@ -19,6 +26,7 @@ class StencilTest(unittest.TestCase):
         self.assertAlmostEqual( es[( 0,)], -2)
         self.assertAlmostEqual( es[( 1,)],  1)
         self.assertEqual(len(es), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
